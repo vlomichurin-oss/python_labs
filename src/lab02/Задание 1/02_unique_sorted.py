4e23b8650cbcd3ad232022f25337bf36e4eabb41
vlomichurin-oss <vlomichurin@gmail.com>
2025-09-24 15:13:16 +0300

    Delete 02_unique_sorted.py

--- src/lab02/Задание 1/02_unique_sorted.py
+++
@@ -1,7 +0,0 @@
-def unique_sorted(nums: list[float | int]) -> list[float | int]:
-    return sorted(set(nums))
-
-print(unique_sorted([3, 1, 2, 1, 3]))
-print(unique_sorted([]))
-print(unique_sorted([-1, -1, 0, 2, 2]))
-print(unique_sorted([1.0, 1, 2.5, 0]))
