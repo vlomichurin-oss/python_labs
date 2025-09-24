c05005960355c6a64186b1e1e9060643a9c19983
vlomichurin-oss <vlomichurin@gmail.com>
2025-09-24 15:40:44 +0300

    Update transpose.py

--- src/lab02/matrix/transpose.py
+++ src/lab02/matrix/transpose.py
@@ -1,4 +1,13 @@
 def transpose(mat: list[list[float | int]]) -> list[list]:
     if not mat:
         return []
-    
+
+    if any(len(row) != len(mat[0]) for row in mat):
+        raise ValueError
+    return [list(column) for column in zip(*mat)]
+
+print(transpose([[1, 2, 3]]))
+print(transpose([[1], [2], [3]]))
+print(transpose([[1, 2], [3, 4]]))
+print(transpose([]))
+print(transpose([[1, 2], [3]]))
