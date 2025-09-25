38b0034fddd1fa4bd047c8eca94504a9acd88049
vlomichurin-oss <vlomichurin@gmail.com>
2025-09-25 10:09:45 +0300

    Update row_sums.py

--- src/lab02/matrix/row_sums.py
+++ src/lab02/matrix/row_sums.py
@@ -1,2 +1,14 @@
 def row_sums(mat: list[list[float | int]]) -> list[float]:
-    
+    if not mat:
+        return []
+
+    row_lenght = len(mat[0])
+    for row in mat:
+        if len(row) != row_lenght:
+            raise ValueError
+    return [sum(row) for row in mat]
+
+print(row_sums([[1, 2, 3], [4, 5, 6]]))
+print(row_sums([[-1, 1], [-10, 10]]))
+print(row_sums([[0, 0], [0, 0]]))
+print(row_sums([[1, 2], [3]]))
