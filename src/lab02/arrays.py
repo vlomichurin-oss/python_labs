977b9d39002cb77b5293329ad32ad69f4574f4a2
vlomichurin-oss <vlomichurin@gmail.com>
2025-10-05 22:14:57 +0300

    Update arrays.py

--- src/lab02/arrays.py
+++ src/lab02/arrays.py
@@ -15,18 +15,18 @@ def flatten(mat: list[list | tuple]) -> list:
     return result
 
 print("min_max:")
-print(min_max([3, -1, 5, 5, 0]))  # (-1, 5)
-print(min_max([42]))  # (42, 42)
-print(min_max([-5, -2, -9]))  # (-9, -2)
-print(min_max([1.5, 2, 2.0, -3.1]))  # (-3.1, 2)
+print(min_max([3, -1, 5, 5, 0]))
+print(min_max([42]))
+print(min_max([-5, -2, -9]))
+print(min_max([1.5, 2, 2.0, -3.1]))
 
 print("unique_sorted:")
-print(unique_sorted([3, 1, 2, 1, 3]))  # [1, 2, 3]
-print(unique_sorted([]))  # []
-print(unique_sorted([-1, -1, 0, 2, 2]))  # [-1, 0, 2]
-print(unique_sorted([1.0, 1, 2.5, 2.5, 0]))  # [0, 1, 2.5]
+print(unique_sorted([3, 1, 2, 1, 3]))
+print(unique_sorted([]))
+print(unique_sorted([-1, -1, 0, 2, 2]))
+print(unique_sorted([1.0, 1, 2.5, 2.5, 0]))
 
 print("flatten:")
-print(flatten([[1, 2], [3, 4]]))  # [1, 2, 3, 4]
-print(flatten([[1, 2], (3, 4, 5)]))  # [1, 2, 3, 4, 5]
-print(flatten([[1], [], [2, 3]]))  # [1, 2, 3]
+print(flatten([[1, 2], [3, 4]]))
+print(flatten([[1, 2], (3, 4, 5)]))
+print(flatten([[1], [], [2, 3]]))
