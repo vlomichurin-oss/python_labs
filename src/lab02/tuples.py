183d7cd5b1ae4d4d66553af1d17c99db77465b0e
vlomichurin-oss <vlomichurin@gmail.com>
2025-10-07 13:11:55 +0300

    Update tuples.py

--- src/lab02/tuples.py
+++ src/lab02/tuples.py
@@ -10,10 +10,12 @@ def format_record(rec: tuple[str, str, float]) -> str:
     initials = ''.join(f"{name[0].upper()}." for name in parts[1:])
 
     gpa_str = f"{gpa:.2f}"
-
-    return f"{surname} {initials}, гр. {group_clean}, GPA {gpa_str}"
-
+    mass = [surname, initials, gpa_str]
+    if len(mass) == 3:
+        return f"{surname} {initials}, гр. {group_clean}, GPA {gpa_str}"
+    else:
+        return TypeError
 print(format_record(("Иванов Иван Иванович", "BIVT-25", 4.6)))
 print(format_record(("Петров Пётр", "IKBO-12", 5.0)))
 print(format_record(("Петров Пётр Петрович", "IKBO-12", 5.0)))
-print(format_record(("  сидорова  анна   сергеевна ", "ABB-01", 3.999)))
+print(format_record(("  сидорова  анна   сергеевна ", "ABB-01", 3.999Í)))
