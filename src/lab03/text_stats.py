7a199750012c17f28a0790323f8601dce7ee930c
vlomichurin-oss <vlomichurin@gmail.com>
2025-10-14 13:20:22 +0300

    Update text_stats.py

--- src/lab03/text_stats.py
+++ src/lab03/text_stats.py
@@ -1,8 +1,17 @@
 import sys
+sys.path.append('/Users/edna/Desktop/python_labs/src/lib')
+from src.lib import text as text
 
-sys.path.append('/Users/edna/Desktop/python_labs/src/lib/text.py')
+string = sys.stdin.readline()
+tokenized = text.tokenize(string)
+unique_words = text.count_freq(tokenized)
+print(f"Всего слов: {len(tokenized)}")
+print(f"Уникальных слов: {len(unique_words)}")
+print(f"Топ-5:")
+k = text.top_n(unique_words)
+for token in k:
+    print(token[0] + ":" + str(token[1]))
 
-from text import *
 
 
 
